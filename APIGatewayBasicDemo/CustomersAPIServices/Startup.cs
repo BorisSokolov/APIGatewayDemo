4bf7be1e88cc5452054efe740e64d8cb4d31ea3f
Borys Sokolov <Borys.Sokolov@infopulse.com>
2019-12-31 14:55:38 +0200

    Updating the Services to .Net Core 3.0

--- APIGatewayBasicDemo/CustomersAPIServices/Startup.cs
+++ APIGatewayBasicDemo/CustomersAPIServices/Startup.cs
@@ -1,11 +1,11 @@
-﻿namespace CustomerAPIServices
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace CustomerAPIServices
 {
-    using Microsoft.AspNetCore.Mvc;
-    using Microsoft.AspNetCore.Builder;
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Configuration;
-    using Microsoft.Extensions.DependencyInjection;
-    
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -15,19 +15,30 @@
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddControllers();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseMvc();
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
         }
     }
 }
